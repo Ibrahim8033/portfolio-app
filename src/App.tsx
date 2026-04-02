ab0c02930f635116b74247aa774ba86f65269415
Saquibs7 <saquibhussain1289@gmail.com>
2026-04-03 00:34:16 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-900 p-40 m-40 rounded-xl flex justify-center item-center text-bold text-white'>
+        Ibrahim Khan Comming SOON BUDDY.....
+kjhdls
+        hi there
+      </div>
+          
+    </>
+  )
+}
+
+export default App
